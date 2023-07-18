f83137f3aa503dfc3f5931f27db95d7385e52489
Donika Sela <donika@donikas-mbp.mynetworksettings.com>
2023-07-17 22:05:51 -0400

    Adding Blogs To HomePage

--- blogproject/Controllers/HomeController.cs
+++ blogproject/Controllers/HomeController.cs
@@ -1,21 +1,27 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using blogproject.Models;
+using blogproject.Repositories;
 
 namespace blogproject.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly IBlogPostRepository blogPostRepository;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IBlogPostRepository blogPostRepository)
     {
         _logger = logger;
+        this.blogPostRepository = blogPostRepository;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var blogPosts = await blogPostRepository.GetAllAsync();
+
+
+        return View(blogPosts); 
     }
 
     public IActionResult Privacy()
